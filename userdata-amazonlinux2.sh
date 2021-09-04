31db7e2ff26c91958abbd66a5e5fe43615882fc4
Roshan <v.c.roshan@gmail.com>
2021-09-04 22:33:07 +0530

    Create userdata-amazonlinux2.sh

---
+++ userdata-amazonlinux2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#updating amazon linux patches
+sudo yum update -y
+#installing docker engine
+sudo amazon-linux-extras install docker -y
+#starting docker service
+sudo service docker start
+#Adding user ec2-user to docker group
+sudo usermod -a -G docker ec2-user
+#enabling docker service
+sudo chkconfig docker on
+#downloading docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#changing the permissions
+sudo chmod +x /usr/local/bin/docker-compose
+
+systemctl restart docker
