0a62514049aab6d17b18da63867d4e11f2f94a59
Roshan <v.c.roshan@gmail.com>
2023-01-10 10:10:18 +0530

    Update userdata-amazonlinux2.sh

--- userdata-amazonlinux2.sh
+++ userdata-amazonlinux2.sh
@@ -10,7 +10,7 @@ sudo usermod -a -G docker ec2-user
 #enabling docker service
 sudo chkconfig docker on
 #downloading docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 #changing the permissions
 sudo chmod +x /usr/local/bin/docker-compose
 
